ad7d33e403a91b7cfacfd68d7cabff7849a67d45
Paulo Ragonha <paulo@ragonha.me>
2014-04-03 23:07:39 -0300

    reorganizes assets

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@
 # the following line to use "https"
 source 'http://rubygems.org'
 
-gem "middleman", "~>3.0.12"
+gem "middleman", "~>3.1"
 gem "middleman-blog"
 gem "nokogiri"
 gem "middleman-syntax"
--- config.rb
+++ config.rb
@@ -47,11 +47,10 @@
 #   end
 # end
 
-set :css_dir, 'stylesheets'
-
-set :js_dir, 'javascripts'
-
-set :images_dir, 'images'
+set :css_dir, 'assets/stylesheets'
+set :js_dir, 'assets/javascripts'
+set :images_dir, 'assets/images'
+set :fonts_dir, 'assets/fonts'
 
 activate :syntax
 set :markdown_engine, :redcarpet
